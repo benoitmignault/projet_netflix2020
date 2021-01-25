8278afe37a2648bbb0e339cfd26dfb48e72bc686
Benoit Mignault <b.mignault@gmail.com>
2021-01-24 20:20:35 -0500

    Création d'un page graphique de base

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from modules.fonction import *
+import tkinter as tk
 
 une_personne = creation_personne("Thibault", "Anita", "M")
 
@@ -6,3 +7,10 @@ print(une_personne.prenom)
 print(une_personne.nom)
 print(une_personne.sexe)
 print(une_personne.id)
+
+root = tk.Tk()
+
+w = tk.Label(root, text="Hello Tkinter!")
+w.pack()
+
+root.mainloop()
